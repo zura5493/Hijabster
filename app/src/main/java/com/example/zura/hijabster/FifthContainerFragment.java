3d836c8d3c35942e49fdbaa648ed46d1b31fb5aa
zura5493 <hitomie_nakamura@yahoo.com>
2015-05-13 21:41:16 +0800

    Initial commit

---
+++ app/src/main/java/com/example/zura/hijabster/FifthContainerFragment.java
@@ -0,0 +1,32 @@
+package com.example.zura.hijabster;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * Created by zura on 5/12/2015.
+ */
+public class FifthContainerFragment extends BaseContainerFragment{
+    private boolean mIsViewInited;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.container_fragment, null);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        if (!mIsViewInited) {
+            mIsViewInited = true;
+            initView();
+        }
+    }
+
+    private void initView() {
+        replaceFragment(new FifthFragment(), false);
+    }
+}
